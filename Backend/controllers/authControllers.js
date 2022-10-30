f2091934a3fbcbc14e919a41626d44419e120256
Adrion10 <lacehila@gmail.com>
2022-10-30 23:07:58 +0100

    login

--- Backend/controllers/authControllers.js
+++ Backend/controllers/authControllers.js
@@ -1,5 +1,6 @@
 import User from "../models/userModal.js";
 import bcrypt from "bcryptjs";
+import { createError } from "../utils/error.js";
 
 export const register = async (req, res, next) => {
   try {
@@ -14,6 +15,23 @@ export const register = async (req, res, next) => {
 
     await newUser.save();
     res.status(200).send("User has been created.");
+  } catch (err) {
+    next(err);
+  }
+};
+export const login = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) return next(createError(404, "User not found"));
+
+    const isPasswordCorrect = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!isPasswordCorrect)
+      return next(createError(400, "Wrong password or Username"));
+
+    res.status(200).json(user);
   } catch (err) {
     next(err);
   }
--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { register } from "../controllers/authControllers.js";
+import { register, login } from "../controllers/authControllers.js";
 
 const router = express.Router();
 
 router.post("/register", register);
+router.post("/login", login);
 
 export default router;
