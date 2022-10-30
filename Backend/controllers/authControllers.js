fb5f24aed145fb6b3c6702ff2f7b395e08746826
Adrion10 <lacehila@gmail.com>
2022-10-30 23:21:10 +0100

    password

--- Backend/controllers/authControllers.js
+++ Backend/controllers/authControllers.js
@@ -30,8 +30,8 @@ export const login = async (req, res, next) => {
     );
     if (!isPasswordCorrect)
       return next(createError(400, "Wrong password or Username"));
-
-    res.status(200).json(user);
+    const { password, isAdmin, ...otherDetails } = user._doc;
+    res.status(200).json({ otherDetails });
   } catch (err) {
     next(err);
   }
