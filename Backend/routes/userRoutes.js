99547c1075f3f2342765747bcd2f08d8d13066db
Adrion10 <lacehila@gmail.com>
2022-10-31 17:54:33 +0100

    router.get("/checkauthenticaion", verifyToken, (req, res, next) => {   res.send("hello user, you are logged in!"); });

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -5,9 +5,13 @@ import {
   deleteUser,
   getUser,
   getUsers,
-} from "../controllers/UserControllers.js";
+} from "../controllers/userControllers.js";
+import { verifyToken } from "../utils/verifyToken.js";
 // import { createError } from "../utils/error.js";
 
+router.get("/checkauthenticaion", verifyToken, (req, res, next) => {
+  res.send("hello user, you are logged in!");
+});
 // Update
 router.put("/:id", updateUser);
 
