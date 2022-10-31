ecd55ded8a758a02de2f27df24c4e04efa1ad9be
Adrion10 <lacehila@gmail.com>
2022-10-31 17:30:56 +0100

    userRouter.js

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,5 +1,23 @@
 import express from "express";
-
 const router = express.Router();
+import {
+  updateUser,
+  deleteUser,
+  getUser,
+  getUsers,
+} from "../controllers/UserControllers.js";
+// import { createError } from "../utils/error.js";
+
+// Update
+router.put("/:id", updateUser);
+
+// Delete
+router.delete("/:id", deleteUser);
+
+//Get
+router.get("/:id", getUser);
+
+//get all
+router.get("/", getUsers);
 
 export default router;
---
+++ Backend/utils/verifyToken.js
@@ -0,0 +1,6 @@
+import jwt from "jsonwebtoken";
+import { createError } from "..utils/error.js";
+
+export const verifyToken = (req, res, next) => {
+    const
+};
