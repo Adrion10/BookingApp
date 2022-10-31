25d6edc548b705dd6cd9bb94e3873e3e06009824
Adrion10 <lacehila@gmail.com>
2022-10-31 22:58:48 +0100

    metod e shkurter

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -9,25 +9,25 @@ import {
 import { verifyToken, verifyUser, verifyAdmin } from "../utils/verifyToken.js";
 // import { createError } from "../utils/error.js";
 
-router.get("/checkauthenticaion", verifyToken, (req, res, next) => {
-  res.send("hello user, you are logged in!");
-});
-router.put("/checkuser:id", verifyUser, (req, res, next) => {
-  res.send("hello user, you are logged in and you can delete your account!");
-});
-router.put("/checkadmin:id", verifyAdmin, (req, res, next) => {
-  res.send("hello Admin, you are logged in and you can delete all account!");
-});
+// router.get("/checkauthenticaion", verifyToken, (req, res, next) => {
+//   res.send("hello user, you are logged in!");
+// });
+// router.put("/checkuser:id", verifyUser, (req, res, next) => {
+//   res.send("hello user, you are logged in and you can delete your account!");
+// });
+// router.put("/checkadmin:id", verifyAdmin, (req, res, next) => {
+//   res.send("hello Admin, you are logged in and you can delete all account!");
+// });
 // Update
-router.put("/:id", updateUser);
+router.put("/:id", verifyUser, updateUser);
 
 // Delete
-router.delete("/:id", deleteUser);
+router.delete("/:id", verifyUser, deleteUser);
 
 //Get
-router.get("/:id", getUser);
+router.get("/:id", verifyUser, getUser);
 
 //get all
-router.get("/", getUsers);
+router.get("/", verifyAdmin, getUsers);
 
 export default router;
