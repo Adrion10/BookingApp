c102c44f077e148e9d4bc58a5444e1af77d159c7
Adrion10 <lacehila@gmail.com>
2022-11-01 22:12:38 +0100

    Hotel and list component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home/Home";
+import List from "./pages/list/List";
+import Hotel from "./pages/hotel/Hotel";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/hotels" element={<List />} />
+        <Route path="/hotels/:id" element={<Hotel />} />
       </Routes>
     </Router>
   );
