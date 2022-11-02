64e3177def9a0bacece2a5a4c718ac52642eb0ca
Adrion10 <lacehila@gmail.com>
2022-11-02 22:08:06 +0100

    header

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,42 +1,70 @@
-import "./header.css"
-import {faBed, faPlane,faCar,faTaxi} from "@fortawesome/free-solid-svg-icons"
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import "./header.css";
+import {
+  faBed,
+  faPlane,
+  faCar,
+  faTaxi,
+  faCalendarDays,
+  faPerson,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Header = () => {
   return (
     <div className="header">
       <div className="headerContainer">
         <div className="headerList">
-        <div className="headerListItem active">
-        <FontAwesomeIcon icon={faBed } />
-        <span>Stays</span>
+          <div className="headerListItem active">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Stays</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faPlane} />
+            <span>Flights</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faCar} />
+            <span>Car rentals</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Attractions</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faTaxi} />
+            <span>Airport taxis</span>
+          </div>
         </div>
-        <div className="headerListItem">
-        <FontAwesomeIcon icon={faPlane } />
-        <span>Flights</span>
+        <h1 className="headerTitle">
+          When it comes to booking a holiday, we know everyone likes something
+          different{" "}
+        </h1>
+        <p className="headerDesc">
+          Get rewarded for your travels - unlock instant savings of 10% or month
+          with a free laceBooking account
+        </p>
+        <button className="headerBtn">Sign in / Register</button>
+        <div className="headerSearch">
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faBed} className="headerIcon" />
+            <input
+              type="text"
+              placeholder="Wher are you going?"
+              className="headerSearchInput"
+            />
+          </div>
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faCalendarDays} className="headerIcon" />
+            <span className="headerSearchText">date to date</span>
+          </div>
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faPerson} className="headerIcon" />
+            <span className="headerSearchText">2 Adults 2 children 1 room</span>
+          </div>
         </div>
-        <div className="headerListItem">
-        <FontAwesomeIcon icon={faCar } />
-        <span>Car rentals</span>
-        </div>
-        <div className="headerListItem">
-        <FontAwesomeIcon icon={faBed } />
-        <span>Attractions</span>
-        </div>
-        <div className="headerListItem">
-        <FontAwesomeIcon icon={faTaxi } />
-        <span>Airport taxis</span>
-        </div>
-      </div> 
-      <h1 className="headerTitle">When it comes to booking a holiday, we know everyone likes something different </h1>
-      <p className="headerDesc">Get rewarded for your travels - unlock instant savings of 10% or month with a free laceBooking account</p>
-      <button className="headerBtn">Sign in / Register</button>
-      <div className="headerSearched">
-        
       </div>
-    </div> 
-  </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Header
+export default Header;
