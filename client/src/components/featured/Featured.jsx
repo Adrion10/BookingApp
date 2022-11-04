90575625b906b037bc1300eed4b0f887b599fd80
Adrion10 <lacehila@gmail.com>
2022-11-04 21:31:29 +0100

    home

---
+++ client/src/components/featured/Featured.jsx
@@ -0,0 +1,43 @@
+import "./featured.css";
+
+const Featured = () => {
+  return (
+    <div className="featured">
+      <div className="featuredItem">
+        <img
+          src="https://images.unsplash.com/photo-1610016302534-6f67f1c968d8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1550&q=80"
+          alt=""
+          className="featuresImg"
+        />
+        <div className="featuredTitles">
+          <h1>Berlin</h1>
+          <h2>123 propertis</h2>
+        </div>
+      </div>
+      <div className="featuredItem">
+        <img
+          src="https://images.unsplash.com/photo-1530284610319-31ee7c55378e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80"
+          alt=""
+          className="featuresImg"
+        />
+        <div className="featuredTitles">
+          <h1>Vien</h1>
+          <h2>123 propertis</h2>
+        </div>
+      </div>
+      <div className="featuredItem">
+        <img
+          src="https://images.unsplash.com/photo-1508035460735-91088c495500?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
+          alt=""
+          className="featuresImg"
+        />
+        <div className="featuredTitles">
+          <h1>Paris</h1>
+          <h2>123 propertis</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,16 +1,18 @@
-import "./home.css"
-import Navbar from "../../components/navbar/Navbar"
-import Header from "../../components/header/Header"
+import "./home.css";
+import Navbar from "../../components/navbar/Navbar";
+import Header from "../../components/header/Header";
+import Featured from "../../components/featured/Featured";
 
 const Home = () => {
   return (
-    
     <div>
-        <Navbar/>
-        <Header /> 
-    
+      <Navbar />
+      <Header />
+      <div className="homeContainer">
+        <Featured />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
