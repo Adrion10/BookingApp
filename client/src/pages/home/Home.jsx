f55ac2ef34488932748d518493eab0feb43c8c81
Adrion10 <lacehila@gmail.com>
2022-11-05 18:30:47 +0100

    ProprtyList

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import "./home.css";
 import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
 import Featured from "../../components/featured/Featured";
+import PropertyListener from "../../components/propertyList/PropertyList";
 
 const Home = () => {
   return (
@@ -10,6 +11,8 @@ const Home = () => {
       <Header />
       <div className="homeContainer">
         <Featured />
+        <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyListener />
       </div>
     </div>
   );
