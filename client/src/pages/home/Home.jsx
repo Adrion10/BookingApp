c82661bf9a1f3e7613facfb95af43c609d2c110b
Adrion10 <lacehila@gmail.com>
2022-11-06 19:54:26 +0100

    featuredProperties

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
 import Featured from "../../components/featured/Featured";
 import PropertyListener from "../../components/propertyList/PropertyList";
+import FeaturedProperty from "../../components/featuredProperties/featuredProperty";
 
 const Home = () => {
   return (
@@ -14,6 +15,7 @@ const Home = () => {
         <h1 className="homeTitle">Browse by property type</h1>
         <PropertyListener />
         <h1 className="homeTitle">Homes guests love</h1>
+        <FeaturedProperty />
       </div>
     </div>
   );
