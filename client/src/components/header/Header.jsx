2266ed411433647240693778fe5ade4b0dc1c622
Adrion10 <lacehila@gmail.com>
2022-11-06 23:12:33 +0100

    search

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -6,6 +6,7 @@ import {
   faPlane,
   faTaxi,
 } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./header.css";
 import { DateRange } from "react-date-range";
@@ -24,11 +25,14 @@ const Header = ({ type }) => {
     },
   ]);
   const [openOptions, setOpenOptions] = useState(false);
+  const [destination, setDestination] = useState("");
   const [options, setOptions] = useState({
     adult: 1,
     children: 0,
     room: 1,
   });
+  const navigate = useNavigate();
+
   const handleOption = (name, operation) => {
     setOptions((prev) => {
       return {
@@ -37,7 +41,9 @@ const Header = ({ type }) => {
       };
     });
   };
-
+  const handleSearch = () => {
+    navigate("/hotels", { state: { destination, date, options } });
+  };
   return (
     <div className="header">
       <div
@@ -84,6 +90,7 @@ const Header = ({ type }) => {
                   type="text"
                   placeholder="Where are you going?"
                   className="headerSearchInput"
+                  onChange={(e) => setDestination(e.target.value)}
                 />
               </div>
               <div className="headerSearchItem">
@@ -181,7 +188,9 @@ const Header = ({ type }) => {
                 )}
               </div>
               <div className="headerSearchItem">
-                <button className="headerBtn">Search</button>
+                <button className="headerBtn" onClick={handleSearch}>
+                  Search
+                </button>
               </div>
             </div>
           </>
--- client/src/pages/list/List.jsx
+++ client/src/pages/list/List.jsx
@@ -1,8 +1,15 @@
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
+import { format } from "date-fns";
 import "./list.css";
 
 const List = () => {
+  const location = useLocation();
+  const [destination, setDestination] = useState(location.state.destination);
+  const [date, setDate] = useState(location.state.date);
+  const [options, setOptions] = useState(location.state.options);
   return (
     <div>
       <Navbar />
@@ -11,6 +18,17 @@ const List = () => {
         <div className="listWrapper">
           <div className="listSearch">
             <h1 className="lsTitleSearch">Search</h1>
+            <div className="lsItem">
+              <label> Destination</label>
+              <input type="text" placeholder={destination} />
+            </div>
+            <div className="lsItem">
+              <label> Chek-in Date</label>
+              <span>{`${format(date[0].startDate, "MM/dd/yyyy")} to ${format(
+                date[0].endDate,
+                "MM/dd/yyyy"
+              )}`}</span>
+            </div>
           </div>
           <div className="listResult"></div>
         </div>
