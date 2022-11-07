88acce96c0a22dbc1a6c7ac1616d0da7926c2c71
Adrion10 <lacehila@gmail.com>
2022-11-07 21:42:43 +0100

    fixed

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -16,7 +16,7 @@ import { format } from "date-fns";
 import { useNavigate } from "react-router-dom";
 
 const Header = ({ type }) => {
-  const [destination, setDestination] = useState();
+  const [destination, setDestination] = useState("");
   const [openDate, setOpenDate] = useState(false);
   const [date, setDate] = useState([
     {
