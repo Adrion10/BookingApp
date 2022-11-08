c0b07d47b1992c819f07d60cb30b6f21395681d2
Adrion10 <lacehila@gmail.com>
2022-11-08 15:44:23 +0100

    hotelDetails

--- client/src/pages/hotel/Hotel.jsx
+++ client/src/pages/hotel/Hotel.jsx
@@ -30,7 +30,7 @@ const Hotel = () => {
             />
           </div>
         )}
-         <div className="hotelWrapper">
+        <div className="hotelWrapper">
           <button className="bookNow"> Book Now!</button>
           <h1 className="hotelTitle">Dusseledorf strasse Apartments</h1>
           <div className="hotelAddress">
@@ -55,6 +55,41 @@ const Hotel = () => {
               </div>
             ))}
           </div>
+          <div className="hotelDetails">
+            <div className="hotelDetailsTexts">
+              <h1 className="hotelTitle">Stay in the heart of City</h1>
+              <p className="hotelDesc">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
+                provident saepe vel. Voluptates assumenda, tempore impedit iste
+                maiores obcaecati vel tempora, sunt aliquam, quo quisquam quam.
+                Velit hic distinctio eum beatae labore libero doloribus dolorum
+                praesentium, voluptatum eaque dolorem explicabo natus
+                perferendis doloremque voluptas magni rem fugiat! Dolorem,
+                consequatur, dignissimos, nemo repellendus dolorum esse
+                veritatis molestias accusamus voluptatum rem sed cum
+                praesentium. Dolorem adipisci, impedit rerum hic vitae odio quos
+                ducimus at obcaecati dolor accusamus maxime, illum aliquam
+                libero, debitis fuga architecto incidunt? Officia ducimus magnam
+                similique nobis? Maiores quae voluptas quibusdam possimus magnam
+                laborum repellat ipsa non obcaecati sunt?
+              </p>
+            </div>
+            <div className="hotelDetailsPrice">
+              <h1>Perfect for a 9-night stay!</h1>
+              <span>
+                Located in the real heart of Krakow, this property has an
+                excellent location score of 9.8!
+              </span>
+              <h2>
+                <b>$945</b> (9 nights)
+              </h2>
+              <button>Reserve or Book Now!</button>
+            </div>
+          </div>
+        </div>
+        <MailList />
+        <Footer />
+      </div>
     </div>
   );
 };
