247d89bf7bdc8964070c42910250dca0a9641c01
Adrion10 <lacehila@gmail.com>
2022-11-08 15:42:41 +0100

    hotelWrapper

--- client/src/pages/hotel/Hotel.jsx
+++ client/src/pages/hotel/Hotel.jsx
@@ -30,6 +30,31 @@ const Hotel = () => {
             />
           </div>
         )}
+         <div className="hotelWrapper">
+          <button className="bookNow"> Book Now!</button>
+          <h1 className="hotelTitle">Dusseledorf strasse Apartments</h1>
+          <div className="hotelAddress">
+            <FontAwesomeIcon icon={faLocationDot} />
+            <span>Berliner St 30 Dusseldorf</span>
+          </div>
+          <span className="hotelDistance">
+            Excellent location – 500m from center
+          </span>
+          <span className="hotelPriceHighlight">
+            Book a stay over €95 at this property and get a free airport taxi
+          </span>
+          <div className="hotelImages">
+            {photos.map((photo, i) => (
+              <div className="hotelImgWrapper" key={i}>
+                <img
+                  onClick={() => handleOpen(i)}
+                  src={photo.src}
+                  alt=""
+                  className="hotelImg"
+                />
+              </div>
+            ))}
+          </div>
     </div>
   );
 };
