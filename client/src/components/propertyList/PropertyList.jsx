2f2ff1ae7d2ca8ee8de5a845e789f5ad548b5f10
Adrion10 <lacehila@gmail.com>
2022-11-10 16:35:52 +0100

    {data[i].count} {data[i].type}

--- client/src/components/propertyList/PropertyList.jsx
+++ client/src/components/propertyList/PropertyList.jsx
@@ -1,52 +1,35 @@
 import "./propertyList.css";
 
 const PropertyList = () => {
+  const { data, loading, error } = useFetch(
+    "/hotels/countByCity?cities=berlin,madrit ,paris"
+  );
+  const images = [
+    "https://images.unsplash.com/photo-1584132967334-10e028bd69f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    "https://images.unsplash.com/photo-1606046604972-77cc76aee944?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
+    "https://images.unsplash.com/photo-1571896349842-33c89424de2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
+    "https://images.unsplash.com/photo-1566073771259-6a8506099945?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  ];
   return (
     <div className="pList">
-      <div className="pListItem">
-        <img
-          src="https://images.unsplash.com/photo-1584132967334-10e028bd69f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-          alt=""
-          className="pListImg"
-        />
-        <div className="pListTitle">
-          <h1>Hotels</h1>
-          <h2>333 Nr Hotels </h2>
-        </div>
-      </div>
-      <div className="pListItem">
-        <img
-          src="https://images.unsplash.com/photo-1606046604972-77cc76aee944?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80"
-          alt=""
-          className="pListImg"
-        />
-        <div className="pListTitle">
-          <h1>Hotels</h1>
-          <h2>333 Nr Hotels </h2>
-        </div>
-      </div>
-      <div className="pListItem">
-        <img
-          src="https://images.unsplash.com/photo-1571896349842-33c89424de2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
-          alt=""
-          className="pListImg"
-        />
-        <div className="pListTitle">
-          <h1>Hotels</h1>
-          <h2>333 Nr Hotels </h2>
-        </div>
-      </div>
-      <div className="pListItem">
-        <img
-          src="https://images.unsplash.com/photo-1566073771259-6a8506099945?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-          alt=""
-          className="pListImg"
-        />
-        <div className="pListTitle">
-          <h1>Hotels</h1>
-          <h2>333 Nr Hotels </h2>
-        </div>
-      </div>
+      {loading ? (
+        "loading"
+      ) : (
+        <>
+          {data &&
+            images.map((img, i) => (
+              <div className="pListItem">
+                <img src={img} alt="" className="pListImg" />
+                <div className="pListTitle">
+                  <h1>{data[i].type}</h1>
+                  <h2>
+                    {data[i].count} {data[i].type}{" "}
+                  </h2>
+                </div>
+              </div>
+            ))}
+        </>
+      )}
     </div>
   );
 };
