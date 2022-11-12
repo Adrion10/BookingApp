e872d5f8d5b54ba39724aaa3629c806be863932a
Adrion10 <lacehila@gmail.com>
2022-11-12 17:44:56 +0100

    container login

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -8,7 +8,27 @@ const Login = () => {
     password: undefined,
   });
   const { loading, error, dispatch } = useContext(AuthContext);
-  return <div>Login</div>;
+  return (
+    <div className="login">
+      <div className="loContainer">
+        <input
+          type="text"
+          placeholder="username"
+          id="username"
+          onChange={handleChange}
+          className="loInput"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          id="password"
+          onChange={handleChange}
+          className="loInput"
+        />
+        <button className="loButon">Login</button>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
