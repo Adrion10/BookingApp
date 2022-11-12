ba9f62cdab624d0e10f310e201248839b2a981c4
Adrion10 <lacehila@gmail.com>
2022-11-12 17:39:18 +0100

    login

---
+++ client/src/pages/login/Login.jsx
@@ -0,0 +1,14 @@
+import { useState, useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import "./login.css";
+
+const Login = () => {
+  const [credentials, setCredentials] = useState({
+    username: undefined,
+    password: undefined,
+  });
+  const { loading, error, dispatch } = useContext(AuthContext);
+  return <div>Login</div>;
+};
+
+export default Login;
