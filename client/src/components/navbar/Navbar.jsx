3f4ad65cb393fee6ff6a05abfd1bc9ed262adaa9
Adrion10 <lacehila@gmail.com>
2022-11-13 22:19:18 +0100

    header register

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,17 +1,24 @@
 import "./navbar.css";
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
 
 const Navbar = () => {
+  const { user } = useContext(AuthContext);
   return (
     <div className="navbar">
       <div className="navContainer">
         <Link to="/">
           <span className="logo">Lacebooking</span>
         </Link>
-        <div className="navItem">
-          <button className="navButton">Register</button>
-          <button className="navButton">Login</button>
-        </div>
+        {user ? (
+          user.username
+        ) : (
+          <div className="navItem">
+            <button className="navButton">Register</button>
+            <button className="navButton">Login</button>
+          </div>
+        )}
       </div>
     </div>
   );
