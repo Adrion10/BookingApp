cd2efa581fe0f380a4b38822d914704710d05690
Adrion10 <lacehila@gmail.com>
2022-11-13 22:32:03 +0100

    login

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -9,24 +9,27 @@ const Login = () => {
     username: undefined,
     password: undefined,
   });
-  const { user, loading, error, dispatch } = useContext(AuthContext);
+
+  const { loading, error, dispatch } = useContext(AuthContext);
+
   const navigate = useNavigate();
 
   const handleChange = (e) => {
     setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
   };
+
   const handleClick = async (e) => {
     e.preventDefault();
     dispatch({ type: "LOGIN_START" });
-
     try {
       const res = await axios.post("/auth/login", credentials);
-      dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+      dispatch({ type: "LOGIN_SUCCESS", payload: res.data.details });
       navigate("/");
     } catch (err) {
-      dispatch({ type: "LOGIN_FAILURE", payload: err.respons.data });
+      dispatch({ type: "LOGIN_FAILURE", payload: err.response.data });
     }
   };
+
   return (
     <div className="login">
       <div className="loContainer">
@@ -44,10 +47,10 @@ const Login = () => {
           onChange={handleChange}
           className="loInput"
         />
-        <button className="loButton" disabled={loading} onClick={handleClick}>
+        <button disabled={loading} onClick={handleClick} className="loButton">
           Login
         </button>
-        {error && <span>{error.message} </span>}
+        {error && <span>{error.message}</span>}
       </div>
     </div>
   );
